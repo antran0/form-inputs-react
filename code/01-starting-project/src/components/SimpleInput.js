96bb0a90400c9062c60311bc64010d9d38724194
Steven Tran <steven.tran550@gmail.com>
2023-05-02 14:46:16 -0700

    hooked up state to name input

--- code/01-starting-project/src/components/SimpleInput.js
+++ code/01-starting-project/src/components/SimpleInput.js
@@ -1,12 +1,31 @@
+import React, { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(`Submitting form! Name: ${enteredName}`);
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
